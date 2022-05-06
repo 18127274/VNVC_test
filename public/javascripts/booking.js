17c94be8b4306d3919d63e298e310dc3aab36fec
18127274 <51258104+18127274@users.noreply.github.com>
2022-05-06 22:08:43 +0700

    dang ky tiem

--- public/javascripts/booking.js
+++ public/javascripts/booking.js
@@ -14,120 +14,6 @@
 
 
 
-function get_allService(callback) {
-  var array = [];
-  GET('http://localhost:8081/api/sanpham').then(res =>
-    res.json().then(data => {
-
-      var template = $('#service-table').html();
-      var compiled = Handlebars.compile(template);
-
-      var contextualHtml = compiled({ allservices: data });
-      $('#allservices').html(contextualHtml);
-      array = data;
-      console.log(array);
-      return callback(array);
-    })
-  );
-}
-
-function view_cartof_customer(id_cus, callback) {
-  var array = [];
-  GET('http://localhost:8080/api/xemgiohang/' + id_cus).then(res =>
-    res.json().then(data => {
-
-      var template = $('#service-table').html();
-      var compiled = Handlebars.compile(template);
-
-      var contextualHtml = compiled({ allservices: data });
-      $('#allservices').html(contextualHtml);
-      array = data;
-      console.log(array);
-      return callback(array);
-    })
-  );
-}
-
-
-
-function Register_account_Customer(name, username, pass, phone, email, address, area) {
-  POST('http://localhost:8081/api/khachhang', {
-    "Hoten": name,
-    "Tendangnhap": username,
-    "Matkhau": pass,
-    "Sdt": phone,
-    "Email": email,
-    "Diachi": address,
-    "Mavung": area
-  }).then(res =>
-    res.json().then(data => {
-      if (data != "") {
-        alert("Đăng ký tài khoản thành công");
-        window.location = "http://localhost:8888/login";
-        console.log(data.lenght);
-
-      }
-      else {
-        alert("Đăng ký tài khoản thất bại");
-      }
-    })
-  );
-}
-
-function Register_account_Shipper(name, username, pass, phone, email, address, somuitiem) {
-  POST('http://localhost:8080/api/themshipper', {
-    "hoten": name,
-    "tendangnhap": username,
-    "matkhau": pass,
-    "sdt": phone,
-    "email": email,
-    "diachi": address,
-    "soMuiTiem": somuitiem
-  }).then(res =>
-    res.json().then(data => {
-      console.log(data);
-      if (data != "") {
-        alert("Đăng ký tài khoản thành công");
-        window.location.assign("http://localhost:8888/login_shipper");
-        console.log(data.lenght);
-
-      }
-      else {
-        alert("Đăng ký tài khoản thất bại");
-      }
-
-    })
-  );
-}
-
-function Register_account_supplier(name, username, pass, phone, email, address, somuitiem) {
-  POST('http://localhost:8081/api/nhacungcap', {
-    "hoten": name,
-    "tendangnhap": username,
-    "matkhau": pass,
-    "sdt": phone,
-    "email": email,
-    "diachi": address,
-    "soMuiTiem": somuitiem
-  }).then(res =>
-    res.json().then(data => {
-      console.log(data);
-      if (data != "") {
-        alert("Đăng ký tài khoản thành công");
-        window.location.assign("http://localhost:8888/login_ncc");
-        console.log(data.lenght);
-
-      }
-      else {
-        alert("Đăng ký tài khoản thất bại");
-      }
-
-    })
-  );
-}
-
-
-
 
 
 function delete_service_ofcart(idchitiet) {
@@ -188,23 +74,6 @@ function list_all_service(callback) {
   );
 }
 
-function list_all_service_le(callback) {
-  var array = [];
-
-  GET('http://localhost:8888/vacxin/get_all_vacxin').then(res =>
-    res.json().then(data => {
-
-      var template = $('#service-table').html();
-      var compiled = Handlebars.compile(template);
-
-      var contextualHtml = compiled({ allservices: data });
-      $('#allservices').html(contextualHtml);
-      array = data;
-
-      return callback(array);
-    })
-  );
-}
 
 
 
@@ -268,6 +137,25 @@ function list_all_goivacxin(id, callback) {
     })
   );
 }
+
+function list_all_vacxinle(callback) {
+  var array = [];
+
+  GET('http://localhost:8888/vacxin/get_goivacxin_phanloai/' + id).then(res =>
+    res.json().then(data => {
+
+      var template = $('#service-table').html();
+      var compiled = Handlebars.compile(template);
+
+      var contextualHtml = compiled({ allservices: data });
+      $('#allservices').html(contextualHtml);
+      array = data;
+      console.log(data);
+      return callback(array);
+    })
+  );
+}
+
 
 function get_all_phanloai(callback) {
   var array = [];
@@ -620,10 +508,49 @@ function add_product_into_cart(coookie_id, hoten, gioitinh, sdt, email, diachi, 
 
   }).then(res =>
     res.json().then(data => {
-      console.log(data);
+      if (data.code == 0) {
+        //window.location = "http://localhost:8889/thanhtoan";
+      }
+      else {
+        alert(data.message);
+      }
     })
   );
 }
+
+function add_product_into_cart1(coookie_id, hoten, gioitinh, sdt, email, diachi, ngaysinh, moiquanhe, dsmua, phanloaids, tinhthanh, quanhuyen, phuongxa, trungtam, ngaytiem) {
+  POST('http://localhost:8888/cart/add', {
+    "cookie_id": coookie_id,
+    "NguoiDky": {
+      "HoTen": hoten,
+      "GioiTinh": gioitinh,
+      "SDT": sdt,
+      "Email": email,
+      "DiaChi": diachi,
+      "NgaySinh": ngaysinh,
+      "MoiQH": moiquanhe,
+      "DSMua": dsmua,
+      "PhanLoaiDS": phanloaids,
+      "TinhThanh_ID": tinhthanh,
+      "QuanHuyen_ID": quanhuyen,
+      "PhuongXa_ID": phuongxa,
+      "TrungTam_ID": trungtam,
+      "NgayMuonTiem": ngaytiem
+    }
+
+  }).then(res =>
+    res.json().then(data => {
+      if (data.code == 0) {
+        console.log(data);
+        window.location = "http://localhost:8889/dangkytiem_lienhe";
+      }
+      else {
+        alert(data.message);
+      }
+    })
+  );
+}
+
 /* add_booking(ptthanhtoan, ttthanhtoan, ngaylapdon, phanloaidh, hoten, email, sdt, cmnd, diachi, tinhthanh, quanhuyen, phuongxa, cookie_id); */
 function add_booking(ptthanhtoan, ttthanhtoan, ngaylapdon, phanloaidh, hoten, email, sdt, cmnd, diachi, tinhthanh, quanhuyen, phuongxa, cookie_id) {
   POST('http://localhost:8888/booking/add_booking', {
@@ -645,14 +572,55 @@ function add_booking(ptthanhtoan, ttthanhtoan, ngaylapdon, phanloaidh, hoten, em
 
   }).then(res =>
     res.json().then(data => {
-      console.log("add thanh cong");
-      console.log(data);
-      localStorage.setItem("id_donhang", data.payload.id);
-      console.log(data.payload.id);
-      list_view_cus2();
+      if (data.code == "0") {
+        alert("Đặt hàng thành công");
+        localStorage.setItem("id_donhang", data.payload.id);
+        console.log(data.payload.id);
+        list_view_cus2();
+      }
+      else {
+        /* alert.window("Tài khoản hoặc mật khẩu không đúng, vui lòng nhập lại"); */
+        alert(data.message);
+      }
     })
   );
 }
+
+function add_booking1(ptthanhtoan, ttthanhtoan, ngaylapdon, phanloaidh, hoten, email, sdt, cmnd, diachi, tinhthanh, quanhuyen, phuongxa, cookie_id) {
+  POST('http://localhost:8888/booking/add_booking', {
+    "PhuongThuc": ptthanhtoan,
+    "TinhTrangThanhToan": ttthanhtoan,
+    "NgayLapDon": ngaylapdon,
+    "PhanLoaiDH": phanloaidh,
+    "NguoiLH": {
+      "HoTen": hoten,
+      "Email": email,
+      "SDT": sdt,
+      "CMND": cmnd,
+      "DiaChi": diachi,
+      "TinhThanh_ID": tinhthanh,
+      "QuanHuyen_ID": quanhuyen,
+      "PhuongXa_ID": phuongxa
+    },
+    "CookieID": cookie_id
+
+  }).then(res =>
+    res.json().then(data => {
+      if (data.code == "0") {
+        alert("Đặt hàng thành công");
+        localStorage.setItem("id_donhang", data.payload.id);
+        console.log(data.payload.id);
+      }
+      else {
+        /* alert.window("Tài khoản hoặc mật khẩu không đúng, vui lòng nhập lại"); */
+        alert(data.message);
+      }
+    })
+  );
+}
+
+
+
 
 function add_question(hoten, gioitinh, tuoi, sdt, email1, address, cauhoi, loaicauhoi) {
   POST('http://localhost:8888/support/add_question', {
@@ -672,7 +640,7 @@ function add_question(hoten, gioitinh, tuoi, sdt, email1, address, cauhoi, loaic
       console.log("add thanh cong");
       console.log(data);
       localStorage.setItem("id_question", data.payload.question_id);
-   
+
     })
   );
 }
@@ -684,10 +652,10 @@ function post_answer(id_ques, ans_content, id_hot) {
     "hotline_id": id_hot
   }).then(res =>
     res.json().then(data => {
-      if(data.code == "0"){
-       /*  alert(data.message); */
+      if (data.code == "0") {
+        /*  alert(data.message); */
       }
-      else{
+      else {
         /* alert.window("Tài khoản hoặc mật khẩu không đúng, vui lòng nhập lại"); */
         alert(data.message);
       }
@@ -702,12 +670,12 @@ function login(user, pass) {
   }).then(res =>
     res.json().then(data => {
       console.log(data.code);
-      if(data.code == "0"){
-        window.location = "http://localhost:8889/tuvan_bacsi"; 
+      if (data.code == "0") {
+        window.location = "http://localhost:8889/tuvan_bacsi";
         localStorage.setItem("id_hotline", data.payload.hotline_id);
         localStorage.setItem("name_hotline", data.payload.hotline_name);
       }
-      else{
+      else {
         /* alert.window("Tài khoản hoặc mật khẩu không đúng, vui lòng nhập lại"); */
         alert(data.message);
       }
@@ -717,58 +685,6 @@ function login(user, pass) {
 
 
 
-
-
-
-function update_status_booking(madh, tttt) {
-  PUT('http://localhost:8888/booking/update_payment', {
-    "MaDH": madh,
-    "TinhTrangThanhToan": tttt
-  }).then(res =>
-    res.json().then(data => {
-      console.log(data);
-      if (data != "") {
-        alert("Cập nhật thành công");
-        console.log(data.lenght);
-      }
-      else {
-        alert("Cập nhật thất bại");
-      }
-
-
-    })
-  );
-}
-
-
-
-
-
-
-
-
-
-function Receive_order(mashp, madonhang) {
-  /*  PUT('http://localhost:8080/api/tiepnhandonhang/', {
-     "MaShipper_input": mashp,
-     "MaDH_input": madonhang
-   }).then(res => */
-  PUT('http://localhost:8080/api/tiepnhandonhang?MaShipper_input=' + mashp + '&MaDH_input=' + madonhang).then(res =>
-    res.json().then(data => {
-      console.log(data);
-      if (data != "") {
-        alert("Nhận đơn thành công");
-        console.log(data.lenght);
-
-      }
-      else {
-        alert("Không thể nhận đơn hàng này");
-      }
-
-
-    })
-  );
-}
 
 
 
